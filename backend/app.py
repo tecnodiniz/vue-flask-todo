13c2c4a42f29e8775b37b78994f68a158ec5b17f
Eduardo Diniz <diniz480@gmail.com>
2025-01-09 16:45:17 -0300

    WIP Auth with JWT

--- backend/app.py
+++ backend/app.py
@@ -2,27 +2,32 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify, render_template, send_from_directory, url_for, redirect
 from bson import ObjectId
-from flask_pymongo import PyMongo
 from flask_cors import CORS
+from auth import login, token_require
+from flask_pymongo import PyMongo
 from pymongo.errors import ConnectionFailure
+
 
 load_dotenv()
 
-app = Flask('__name__', static_folder="static")
+app = Flask('__name__', static_folder='static')
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/todo'
 
-# app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+# app.config['MONGO_URI'] = os.getenv('MONGO_URI')
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/todo"
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/todo'
 
-mongo = PyMongo(app)
 
 CORS(app)
 
-@app.route("/")
+mongo = PyMongo(app)
+
+
+@app.route('/')
 def index():
     try:
-        mongo.db.command("ping")
-        return send_from_directory(app.static_folder, "index.html")
+        mongo.db.command('ping')
+        return send_from_directory(app.static_folder, 'index.html')
     except ConnectionFailure:
         return redirect(url_for('error'))
     
@@ -30,24 +35,24 @@ def index():
 # Handle errors
 @app.route('/error')
 def error():
-    return render_template("error.html", message="MongoDB connection failed! Please check the database server.")
+    return render_template('error.html', message='MongoDB connection failed! Please check the database server.')
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"), 404
+    return render_template('404.html'), 404
 
 # POST TASK
-@app.route('/tasks', methods=["POST"])
+@app.route('/tasks', methods=['POST'])
 def create_taks():
     try:
         data = request.json
 
         if data:
             result = mongo.db.list.insert_one(data)
-            return jsonify({"msg": "Taks adicionada", "id": str(result.inserted_id)}), 201
-        return jsonify({"erro":"O formato dos dados é inválido"}), 400
+            return jsonify({'msg': 'Taks adicionada', 'id': str(result.inserted_id)}), 201
+        return jsonify({'erro':'O formato dos dados é inválido'}), 400
     except Exception as e:
-        return jsonify({"error": str(e)})
+        return jsonify({'error': str(e)})
 
 # GET ALL
 @app.route('/tasks')
@@ -58,80 +63,103 @@ def tasks():
 
         if tasks:
             for task in tasks:
-                task["_id"] = str(task["_id"])
+                task['_id'] = str(task['_id'])
         
-        return jsonify({"data":tasks}), 200
+        return jsonify({'data':tasks}), 200
 
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({'error': str(e)}), 500
     
 # GET TASK
-@app.route('/tasks/<id>', methods=["GET"])
+@app.route('/tasks/<id>', methods=['GET'])
 def get_task(id):
     try:
-        if not ObjectId(id):
-            return jsonify({"msg":"Id inválida"})
+        if not ObjectId.isValid(id):
+            return jsonify({'msg':'Id inválida'})
         
-        data = mongo.db.list.find_one({"_id": ObjectId(id)})
+        data = mongo.db.list.find_one({'_id': ObjectId(id)})
 
         if not data:
-            return jsonify({"msg": "Task não encontrada"}), 404
+            return jsonify({'msg': 'Task não encontrada'}), 404
         
-        data["_id"] = str(data["_id"])
-        return jsonify({"task":data}), 200
+        data['_id'] = str(data['_id'])
+        return jsonify({'task':data}), 200
         
     except Exception as e:
-        return jsonify({"error": str(e)})
+        return jsonify({'error': str(e)})
     
 
 # UPDATE TASK
-@app.route('/tasks/<id>', methods=["PUT"])
+@app.route('/tasks/<id>', methods=['PUT'])
 def update_task(id):
     try:
-        if not ObjectId(id):
-            return jsonify({"error": "Id inválida"}),400
+        if not ObjectId.isValid(id):
+            return jsonify({'error': 'Id inválida'}),400
         
         data = request.json
         if not data:
-            return jsonify({"erro":"Nenhum dado inserido para atualização"}),400
+            return jsonify({'erro':'Nenhum dado inserido para atualização'}),400
 
-        result = mongo.db.list.update_one({"_id": ObjectId(id)}, {"$set":data})
+        result = mongo.db.list.update_one({'_id': ObjectId(id)}, {'$set':data})
 
         if result.matched_count == 0:
-            return jsonify({"erro":"Task não encontrada"}),404
+            return jsonify({'erro':'Task não encontrada'}),404
         
-        return jsonify({"msg":"Task atualizada"}), 201
+        return jsonify({'msg':'Task atualizada'}), 201
 
     except Exception as e:
-        return jsonify({"error": str(e)})
+        return jsonify({'error': str(e)})
 
 # DELETE TASK
-@app.route('/tasks/<id>', methods=["DELETE"])
+@app.route('/tasks/<id>', methods=['DELETE'])
 def delete_task(id):
     try:
-        if not ObjectId(id):
-            return jsonify({"msg":"Id inválida"})
+        if not ObjectId.isValid(id):
+            return jsonify({'msg':'Id inválida'})
         
-        result = mongo.db.list.delete_one({"_id": ObjectId(id)})
+        result = mongo.db.list.delete_one({'_id': ObjectId(id)})
 
         if result.deleted_count == 0:
-            return jsonify({"msg": "Taks não encontrada"}), 404
+            return jsonify({'msg': 'Taks não encontrada'}), 404
         
-        return jsonify({"msg": "Task excluida", "id": id}),200
+        return jsonify({'msg': 'Task excluida', 'id': id}),200
         
     except Exception as e:
-        return jsonify({"error": str(e)})
+        return jsonify({'error': str(e)})
+
 
 # DELETE ALL
-@app.route('/tasks/reset', methods=["GET"])
+@app.route('/tasks/reset', methods=['GET'])
+@token_require
 def delete_all():
     try:
         result = mongo.db.list.delete_many({})
 
         if result.deleted_count == 0:
-            return jsonify({"msg":"Nenhum item foi excluido"}),200
-        return jsonify({"msg":"Todos os itens foram excluídos"}), 200
+            return jsonify({'msg':'Nenhum item foi excluido'}),200
+        return jsonify({'msg':'Todos os itens foram excluídos'}), 200
     except Exception as e:
-        return jsonify({"erro": str(e)}), 500
+        return jsonify({'erro': str(e)}), 500
+    
+
+@app.route('/login', methods=['POST'])
+def login_user():
+    try:
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+
+        return login(username,password)
+    except TypeError as te:
+        return jsonify({f'Type error {str(te)}'})
+    except Exception as e:
+        return jsonify({f'An unexpected error occurred {str(e)}'})
+
+
+@app.route('/protected', methods=['GET'])
+@token_require
+def protected_route(current_user):
+    return jsonify({'message':f'Hello, {current_user}! This is a protected route.'})
+
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ backend/auth.py
@@ -0,0 +1,84 @@
+from bson import ObjectId
+import jwt
+from datetime import datetime, timedelta, timezone
+from flask import jsonify, request
+from functools import wraps
+import app
+
+SECRET_KEY = "@@l0ck3d0u7@@"
+
+# Mock Test
+users = {
+    "user1":"password123",
+    "user2":"password456",
+}
+
+#Create JWT Token
+def create_token(user):
+    print(user)
+    expiration_time = datetime.now(timezone.utc) + timedelta(hours=1)
+    payload = {
+        'user_id': user['_id'],
+        'username': user['name'],
+        'exp': expiration_time
+    }
+    token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+    
+    return token
+
+
+# Decorator to protect routes that require authentication
+def token_require(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token = None
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization'].split(' ')[1] # Bearer <token>
+        if not token:
+            return jsonify({"message": "Token is missing!"}), 403
+        
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+            
+            # mongo = app.mongoConnect()
+            
+            if not ObjectId.is_valid(payload["user_id"]):
+                return jsonify({'error': 'Invalid Id'}), 400
+                
+            
+            user = app.mongo.db.usuario.find_one({'_id': ObjectId(payload["user_id"])})
+                
+            if user:
+                current_user = payload['username'] 
+            else:
+                return jsonify({'msg': 'User not found in the database'})
+
+
+        except jwt.ExpiredSignatureError:
+            return jsonify({"message": "Token has expired! Please log in again."}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({"message":"Invalid token! Please log in again."}), 401
+        except Exception as e:
+            return jsonify({"message": f'An error ocurred:{str(e)} '}), 500
+        
+        return f(current_user, *args, ** kwargs)
+    return decorator
+
+#Handle user login and return token if credentials are valid
+def login(username,password):
+    try:
+        user = app.mongo.db.usuario.find_one({'user_login':username, 'pwd':password})
+        if user:
+            user["_id"] = str(user["_id"])
+            token = create_token(user)
+            return jsonify({'token':token}), 200
+        # if username in users and users[username] == password:
+        #     token = create_token(username)
+        #     return jsonify({"token": token}), 200
+        
+        else:
+            return jsonify({"message":"Invalid credentials"}), 401
+    except TypeError as te:
+        return jsonify({f'type error: {str(te)}'})
+    except Exception as e:
+        return jsonify({"message":f"An error ocurred during login: {str(e)}"}),500
